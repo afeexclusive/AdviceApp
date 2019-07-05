32983f6531ad6f6683e7fcf7fc1a469f0ef8f6b0
afeexclusive <afeexclusive@gmail.com>
2019-07-05 14:49:43 -0700

    Add advicefeed page and other features

--- UI/homepage.js
+++ UI/homepage.js
@@ -11,8 +11,8 @@ function changetheme(){
 }
 
 function w3_open() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.width = "30%";
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("openNav").style.display = 'none';
   }
