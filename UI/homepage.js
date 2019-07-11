bf2207e95e1733021e8c9d7f4ac52ca9aa9e3a8f
afeexclusive <afeexclusive@gmail.com>
2019-07-10 18:04:08 -0700

    Add advice feeds using div, others on style

--- UI/homepage.js
+++ UI/homepage.js
@@ -10,7 +10,7 @@ function changetheme(){
     }
 }
 
-function w3_open() {
+  function w3_open() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.width = "30%";
     document.getElementById("mySidebar").style.display = "block";
@@ -37,3 +37,91 @@ function w3_open() {
     if(i<10){i = "0" + 1}
     return i;
   };
+
+  
+  const loadAdvice = async() => {
+    const response = await fetch('http://localhost:3000/question');
+    const data = await response.json();
+    
+    for (var i=0; i<data.length; i++){
+      let divAdFeed = document.createElement('div');
+      let pAdQuest = document.createElement('p');
+      let divAdDetails = document.createElement('div');
+      let spanReply = document.createElement('span');
+      let iReplyCount = document.createElement('i');
+      let spanSame = document.createElement('span');
+      let iSameCount = document.createElement('i');
+      let replyId = data[i]._id;
+      divAdFeed.setAttribute('class', 'w3-white w3-border-bottom w3-padding');
+      pAdQuest.setAttribute('id', 'adquest');
+      spanReply.setAttribute('id', replyId);
+      iReplyCount.setAttribute('id', 'reply');
+      iReplyCount.setAttribute('class', 'w3-badge w3-green');
+      spanSame.setAttribute('class', 'w3-right');
+      pAdQuest.innerHTML = data[i].content;
+      spanReply.appendChild(iReplyCount);
+      divAdDetails.appendChild(spanReply);
+      divAdDetails.appendChild(spanSame);
+      divAdFeed.appendChild(pAdQuest);
+      divAdFeed.appendChild(divAdDetails);
+      iReplyCount.innerHTML = (data.length);
+      spanReply.insertAdjacentText('beforebegin', 'Advice');
+      spanSame.innerText = 'Same here';
+      spanSame.appendChild(iSameCount);
+      iSameCount.insertAdjacentText('afterend', data.length);
+      iSameCount.setAttribute('id', 'same');
+      iSameCount.setAttribute('class', 'w3-badge w3-green');
+      document.getElementById('advicefeed').appendChild(divAdFeed);
+    }
+  }
+function showReply(){
+  document.getElementById('adreply').style.display = 'block';
+}
+
+function closeReply(){
+  document.getElementById('adreply').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+// let table = document.createElement('table');
+//       table.setAttribute('class', 'w3-table w3-bordered w3-striped');
+      
+//       let row = table.insertRow();
+//       for (var i=0; i<data.length; i++){
+//         let cell1 = row.insertCell();
+//         cell1.innerHTML = data[i].content;
+//         cell1.setAttribute('id', data[i].id);
+//         row = table.insertRow();
+//         let cell2 = row.insertCell();
+//         cell2.innerHTML = ('<i><i class="w3-badge w3-green" id="reply"></i>Advice</i>');
+//         row = table.insertRow();
+//       };
+
+
+
+// function loadAdvice(){
+  //   const option = {
+  //     headers: {'Content-Type': 'application/json'},
+  //     method: 'GET',
+  //     mode: 'no-cors', //cors, same-origin, no-cors
+  //   }
+
+  //   fetch('http://localhost:3000/question', option)
+  //   .then(response => response.json())
+  //   .then((data) => {
+  //     let table = document.createElement('table');
+  //     table.setAttribute('class', 'w3-table w3-bordered w3-striped');
+  //     let row = table.insertRow();
+  //     for (var i=0; i<data.length; i++){
+  //       let cell1 = row.insertCell();
+  //       cell1.innerHTML = data[i].content
+  //       cell1.setAttribute('id', data[i].id)
+  //     }
+  //   });
+  // };
--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Headers, Accept");
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Headers, Accept");
+    next();
+  });
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
